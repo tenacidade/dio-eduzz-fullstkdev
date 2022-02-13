fef85862a724371c9c7b081292b8b979d2618384
tenacidade <grd1996@gmail.com>
2022-02-13 00:50:56 -0300

    diferenças tipagem js

---
+++ Aulas/0_Javascript_ES6_essencial/2-tipagem-dinamica.js
@@ -0,0 +1,4 @@
+var minhaVariavel = 30;
+minhaVariavel = 'Texto'
+
+console.log(minhaVariavel) //em js vc pode alterar o tipo de uma variavel de int para string pois é dinamica, mas em java, que não é dinamica, não daria certo.
