d75d85739bd7528d06ac6132481915b455b89508
tenacidade <grd1996@gmail.com>
2022-02-13 00:14:08 -0300

    tipo de tipagem js

---
+++ Aulas/0_Javascript_ES6_essencial/tipagem-fraca.js
@@ -0,0 +1,10 @@
+//O Javascript foi lançado em 1995.
+
+/**
+ * Linguagem de tipagem fraca e dinâmica
+ */
+
+var meuNumero = 20;
+var meuTexto = `Exemplo`;
+
+console.log(meuNumero + meuTexto);  //em javascript concatena porém em Python, daria erro poist a tipagem é forte.
